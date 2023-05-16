f83e1bc1680c04620e7563dd50f995674911e827
1361006944 <1361006944@qq.com>
2023-05-16 15:07:55 +0800

    1

--- start.sh
+++ start.sh
@@ -10,13 +10,12 @@ chmod 777 gdc
 ./gdc --datadir node8/ init node8/genesis.json
 cp service/gdc.services /etc/systemd/system/
 cp service/gdc1.services /etc/systemd/system/
-cp serviceg/dc2.services /etc/systemd/system/
-cp serviceg/dc3.services /etc/systemd/system/
-cp serviceg/dc4.services /etc/systemd/system/
-cp serviceg/dc5.services /etc/systemd/system/
-cp serviceg/dc6.services /etc/systemd/system/
-cp serviceg/dc7.services /etc/systemd/system/
-cp serviceg/dc8.services /etc/systemd/system/
+cp serviceg/gdc2.services /etc/systemd/system/
+cp serviceg/gdc3.services /etc/systemd/system/
+cp serviceg/gdc4.services /etc/systemd/system/
+cp serviceg/gdc5.services /etc/systemd/system/
+cp serviceg/gdc6.services /etc/systemd/system/
+cp serviceg/gdc7.services /etc/systemd/system/
 systemctl start gdc.services
 systemctl enable gdc.services
 journalctl -u gdc.services -f
