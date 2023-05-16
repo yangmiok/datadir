9d3a8e47874af7ecddf71f10e47b48ca746c838e
1361006944 <1361006944@qq.com>
2023-05-16 19:12:29 +0800

    1

--- cpservice.sh
+++ cpservice.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cp service/gdc.services /etc/systemd/system/
-cp service/gdc1.services /etc/systemd/system/
-cp serviceg/gdc2.services /etc/systemd/system/
-cp serviceg/gdc3.services /etc/systemd/system/
-cp serviceg/gdc4.services /etc/systemd/system/
-cp serviceg/gdc5.services /etc/systemd/system/
-cp serviceg/gdc6.services /etc/systemd/system/
-cp serviceg/gdc7.services /etc/systemd/system/
+cp service/gdc.service /etc/systemd/system/
+cp service/gdc1.service /etc/systemd/system/
+cp service/gdc2.service /etc/systemd/system/
+cp service/gdc3.service /etc/systemd/system/
+cp service/gdc4.service /etc/systemd/system/
+cp service/gdc5.service /etc/systemd/system/
+cp service/gdc6.service /etc/systemd/system/
+cp service/gdc7.service /etc/systemd/system/
 systemctl start gdc.services
 systemctl enable gdc.services
 journalctl -u gdc.services -f
